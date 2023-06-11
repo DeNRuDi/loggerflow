ff378c12d8d4eb21100f05d97350e5203f1be61a
den_python <R.Denis.python.wwl@gmail.com>
2023-06-11 15:23:01 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+version = '0.0.1'
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='loggerflow',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/DeNRuDi/loggerflow',
+    author='DeNRuDi, kad1m',
+    author_email='denisrudnitskiy0@gmail.com',
+    description='A new level of bug tracking for your Python projects',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests',
+        'discordwebhook',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
