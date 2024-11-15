fdc24be59677e8bcc3270ee17e033c94b07fa7c1
den_rudi <R.Denis.python.wwl@gmail.com>
2024-11-15 22:30:54 +0200

    added: new version

--- loggerflow/__init__.py
+++ loggerflow/__init__.py
@@ -3,10 +3,11 @@
 :copyright (c) 2024
 """
 
-__author__ = ['DeNRuDi', 'kad1m']
-__version__ = '0.0.5'
-__email__ = ['denisrudnitskiy0@gmail.com', 'sun.moon.kadim@gmail.com']
+__author__ = 'DeNRuDi'
+__version__ = '0.0.6'
+__email__ = 'denisrudnitskiy0@gmail.com'
 __license__ = 'BSD-3-Clause'
 
 
 from .loggerflow import LoggerFlow
+from .traceback_backlighter import TracebackBacklighter
